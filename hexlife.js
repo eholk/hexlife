b84cc6e1ec6c4ff96b41ab755a98ba77a46b3b5f
Eric Holk <eric.holk@gmail.com>
2011-11-23 17:12:49 -0800

    Drew a hexagon with a canvas.

--- hexlife.js
+++ hexlife.js
@@ -1,3 +1,26 @@
-function drawHex(x, y) {
-    
+var ctx = null;
+
+function drawHex(cx, cy, r) {
+    var step = Math.PI / 3;
+    ctx.save();
+    ctx.translate(cx, cy);
+    ctx.scale(r, r);
+    ctx.beginPath();
+    ctx.moveTo(1, 0);
+    for(var i = 1; i <= 6; i++) {
+        ctx.lineTo(Math.cos(step * i), Math.sin(step * i));
+    }
+    ctx.fill();
+    ctx.restore();
+}
+
+function init() {
+    var canvas = document.getElementById("grid");
+    ctx = canvas.getContext("2d");
+}
+
+function start() {
+    init();
+
+    drawHex(320, 240, 60);
 }
